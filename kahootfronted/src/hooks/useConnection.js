27dec8886baab7eaaeaf6b6a74b7280000d3e124
gracjan grzech <g.grzech@icloud.com>
2024-02-13 22:15:38 +0100

    Add hook for establishing a signalr connection

---
+++ kahootfronted/src/hooks/useConnection.js
@@ -0,0 +1,22 @@
+﻿import {useEffect, useState} from "react"
+import * as signalr from "@microsoft/signalr"
+
+const useConnection = () => {
+    const [connection, setConnection] = useState(null)
+    
+    useEffect(() => {
+        const newConnection = new signalr.HubConnectionBuilder()
+            .withUrl("http://localhost:5161/game", 
+                {
+                    skipNegotiation: true,
+                    transport: signalr.HttpTransportType.WebSockets})
+            .withAutomaticReconnect()
+            .build();
+        
+        setConnection(newConnection);
+    }, []);
+    
+    return [connection, setConnection]
+}
+
+export default useConnection;
